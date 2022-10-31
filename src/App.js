afe98f66891576c44087b97666761cc39b6ec58c
Lucca Menezes Luiz <LMLUIZ@SMARAPD.COM.BR>
2022-10-31 14:02:11 -0300

    Criado a pagina de search e dashboard

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import { useState, useEffect } from 'react';
 import { useAuthentication } from './hooks/useAuthentication';
 import CreatePost from './pages/CreatePost/CreatePost';
 import Dashboard from './pages/Dashboard/Dashboard';
+import Search from './pages/Search/Search';
+import Post from './pages/Post/Post';
 
 function App() {
 
@@ -40,6 +42,8 @@ function App() {
             <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/about' element={<About />} />
+              <Route path='/search' element={<Search />} />
+              <Route path='/posts/:id' element={<Post />} />
               <Route path='/login' element={!user ? <Login /> : <Navigate to="/" />} />
               <Route path='/register' element={!user ? <Register /> : <Navigate to="/" />} />
               <Route path='/posts/create' element={user ? <CreatePost /> : <Navigate to="/login" />} />
